62db5ec6bf49f5fec2e16d7f47239447505fb476
Mikko Hellsing <mikko@aino.se>
2017-04-17 02:07:19 +0200

    removing some obsolete code, making add related models work

--- nimda/static/admin/js/admin/RelatedObjectLookups.js
+++ nimda/static/admin/js/admin/RelatedObjectLookups.js
@@ -1,139 +1,158 @@
+'use strict';
+
+/*global SelectBox, interpolate*/
 // Handles related-objects functionality: lookup link for raw_id_fields
 // and Add Another links.
-
-function html_unescape(text) {
-    // Unescape a string that was escaped using django.utils.html.escape.
-    text = text.replace(/&lt;/g, '<');
-    text = text.replace(/&gt;/g, '>');
-    text = text.replace(/&quot;/g, '"');
-    text = text.replace(/&#39;/g, "'");
-    text = text.replace(/&amp;/g, '&');
-    return text;
-}
 
 // IE doesn't accept periods or dashes in the window name, but the element IDs
 // we use to generate popup window names may contain them, therefore we map them
 // to allowed characters in a reversible way so that we can locate the correct
 // element when the popup window is dismissed.
 function id_to_windowname(text) {
-    text = text.replace(/\./g, '__dot__');
-    text = text.replace(/\-/g, '__dash__');
-    return text;
+  text = text.replace(/\./g, '__dot__')
+  text = text.replace(/\-/g, '__dash__')
+  return text
 }
 
 function windowname_to_id(text) {
-    text = text.replace(/__dot__/g, '.');
-    text = text.replace(/__dash__/g, '-');
-    return text;
+  text = text.replace(/__dot__/g, '.')
+  text = text.replace(/__dash__/g, '-')
+  return text
 }
 
-function showAdminPopup(triggeringLink, name_regexp) {
-    var name = triggeringLink.id.replace(name_regexp, '');
-    name = id_to_windowname(name);
-    var href = triggeringLink.href;
-    if (href.indexOf('?') == -1) {
-        href += '?_popup=1';
+function showAdminPopup(triggeringLink, name_regexp, add_popup) {
+  var name = triggeringLink.id.replace(name_regexp, '')
+  name = id_to_windowname(name)
+  var href = triggeringLink.href
+  if (add_popup) {
+    if (href.indexOf('?') === -1) {
+      href += '?_popup=1'
     } else {
-        href  += '&_popup=1';
+      href += '&_popup=1'
     }
-    var win = window.open(href, name, 'height=500,width=800,resizable=yes,scrollbars=yes');
-    win.focus();
-    return false;
+  }
+  var win = window.open(href, name, 'height=500,width=800,resizable=yes,scrollbars=yes')
+  win.focus()
+  return false
 }
 
 function showRelatedObjectLookupPopup(triggeringLink) {
-    return showAdminPopup(triggeringLink, /^lookup_/);
+  return showAdminPopup(triggeringLink, /^lookup_/, true)
 }
 
 function dismissRelatedLookupPopup(win, chosenId) {
-    var name = windowname_to_id(win.name);
-    var elem = document.getElementById(name);
-    if (elem.className.indexOf('vManyToManyRawIdAdminField') != -1 && elem.value) {
-        elem.value += ',' + chosenId;
-    } else {
-        document.getElementById(name).value = chosenId;
-    }
-    win.close();
+  var name = windowname_to_id(win.name)
+  var elem = document.getElementById(name)
+  if (elem.className.indexOf('vManyToManyRawIdAdminField') !== -1 && elem.value) {
+    elem.value += ',' + chosenId
+  } else {
+    document.getElementById(name).value = chosenId
+  }
+  win.close()
 }
 
 function showRelatedObjectPopup(triggeringLink) {
-    var name = triggeringLink.id.replace(/^(change|add|delete)_/, '');
-    name = id_to_windowname(name);
-    var href = triggeringLink.href;
-    var win = window.open(href, name, 'height=500,width=800,resizable=yes,scrollbars=yes');
-    win.focus();
-    return false;
+  return showAdminPopup(triggeringLink, /^(change|add|delete)_/, false)
+}
+
+function updateRelatedObjectLinks(triggeringLink) {
+  var $this = $(triggeringLink)
+  var siblings = $this.nextAll('.change-related, .delete-related')
+  if (!siblings.length) {
+    return
+  }
+  var value = $this.val()
+  if (value) {
+    siblings.each(function() {
+      var elm = $(this)
+      elm.attr('href', elm.attr('data-href-template').replace('__fk__', value))
+    })
+  } else {
+    siblings.removeAttr('href')
+  }
 }
 
 function dismissAddRelatedObjectPopup(win, newId, newRepr) {
-    // newId and newRepr are expected to have previously been escaped by
-    // django.utils.html.escape.
-    newId = html_unescape(newId);
-    newRepr = html_unescape(newRepr);
-    var name = windowname_to_id(win.name);
-    var elem = document.getElementById(name);
-    var o;
-    if (elem) {
-        var elemName = elem.nodeName.toUpperCase();
-        if (elemName == 'SELECT') {
-            // select2 hacked
-            $(elem).select2('destroy')
-            o = new Option(newRepr, newId);
-            elem.options[elem.options.length] = o;
-            o.selected = true;
-            $(elem).select2()
-        } else if (elemName == 'INPUT') {
-            if (elem.className.indexOf('vManyToManyRawIdAdminField') != -1 && elem.value) {
-                elem.value += ',' + newId;
-            } else {
-                elem.value = newId;
-            }
-        }
-        // Trigger a change event to update related links if required.
-        django.jQuery(elem).trigger('change');
-    } else {
-        var toId = name + "_to";
-        o = new Option(newRepr, newId);
-        SelectBox.add_to_cache(toId, o);
-        SelectBox.redisplay(toId);
+  var name = windowname_to_id(win.name)
+  var elem = document.getElementById(name)
+  if (elem) {
+    var elemName = elem.nodeName.toUpperCase()
+    if (elemName === 'SELECT') {
+      elem.options[elem.options.length] = new Option(newRepr, newId, true, true)
+    } else if (elemName === 'INPUT') {
+      if (elem.className.indexOf('vManyToManyRawIdAdminField') !== -1 && elem.value) {
+        elem.value += ',' + newId
+      } else {
+        elem.value = newId
+      }
     }
-    win.close();
+    // Trigger a change event to update related links if required.
+    $(elem).trigger('change')
+  } else {
+    var toId = name + "_to"
+    var o = new Option(newRepr, newId)
+    SelectBox.add_to_cache(toId, o)
+    SelectBox.redisplay(toId)
+  }
+  win.close()
 }
 
 function dismissChangeRelatedObjectPopup(win, objId, newRepr, newId) {
-    objId = html_unescape(objId);
-    newRepr = html_unescape(newRepr);
-    var id = windowname_to_id(win.name).replace(/^edit_/, '');
-    var selectsSelector = interpolate('#%s, #%s_from, #%s_to', [id, id, id]);
-    var selects = django.jQuery(selectsSelector);
-    // select2 hacked
-    $(selects).select2('destroy')
-    selects.find('option').each(function() {
-        if (this.value == objId) {
-            this.innerHTML = newRepr;
-            this.value = newId;
-        }
-    });
-    win.close();
-    $(selects).select2()
-};
+  var id = windowname_to_id(win.name).replace(/^edit_/, '')
+  var selectsSelector = interpolate('#%s, #%s_from, #%s_to', [id, id, id])
+  var selects = $(selectsSelector)
+  $(selects).select2('destroy') // destroy select2 before injecting new value
+  selects.find('option').each(function() {
+    if (this.value === objId) {
+      this.textContent = newRepr
+      this.value = newId
+    }
+  })
+  $(selects).select2({width: '100%'}) // re-apply select2
+  win.close()
+}
 
 function dismissDeleteRelatedObjectPopup(win, objId) {
-    objId = html_unescape(objId);
-    var id = windowname_to_id(win.name).replace(/^delete_/, '');
-    var selectsSelector = interpolate('#%s, #%s_from, #%s_to', [id, id, id]);
-    var selects = django.jQuery(selectsSelector);
-    // select2 hacked
-    $(selects).select2('destroy')
-    selects.find('option').each(function() {
-        if (this.value == objId) {
-            django.jQuery(this).remove();
-        }
-    }).trigger('change');
-    win.close();
-    $(selects).select2()
-};
+  var id = windowname_to_id(win.name).replace(/^delete_/, '')
+  var selectsSelector = interpolate('#%s, #%s_from, #%s_to', [id, id, id])
+  var selects = $(selectsSelector)
+  selects.find('option').each(function() {
+    if (this.value === objId) {
+      $(this).remove()
+    }
+  }).trigger('change')
+  win.close()
+}
 
-// Kept for backward compatibility
-showAddAnotherPopup = showRelatedObjectPopup;
-dismissAddAnotherPopup = dismissAddRelatedObjectPopup;
+$(function() {
+  $("a[data-popup-opener]").click(function(event) {
+    event.preventDefault()
+    opener.dismissRelatedLookupPopup(window, $(this).data("popup-opener"))
+  })
+  $('body').on('click', '.related-widget-wrapper-link', function(e) {
+    e.preventDefault()
+    if (this.href) {
+      var event = $.Event('django:show-related', {href: this.href})
+      $(this).trigger(event)
+      if (!event.isDefaultPrevented()) {
+        showRelatedObjectPopup(this)
+      }
+    }
+  })
+  $('body').on('change', '.related-widget-wrapper select', function(e) {
+    var event = $.Event('django:update-related')
+    $(this).trigger(event)
+    if (!event.isDefaultPrevented()) {
+      updateRelatedObjectLinks(this)
+    }
+  })
+  $('.related-widget-wrapper select').trigger('change')
+  $('body').on('click', '.related-lookup', function(e) {
+    e.preventDefault()
+    var event = $.Event('django:lookup-related')
+    $(this).trigger(event)
+    if (!event.isDefaultPrevented()) {
+      showRelatedObjectLookupPopup(this)
+    }
+  })
+})
